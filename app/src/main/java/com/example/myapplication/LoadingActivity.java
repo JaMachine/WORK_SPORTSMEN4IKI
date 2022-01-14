0922225ec95c79888fdb7400621a558f03f50554
yuriy <yuriy@mail.com>
2022-01-14 16:12:23 +0200

    url check

--- app/src/main/java/com/example/myapplication/LoadingActivity.java
+++ app/src/main/java/com/example/myapplication/LoadingActivity.java
@@ -3,12 +3,14 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,12 +20,15 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 public class LoadingActivity extends AppCompatActivity {
     String fetchedData = "x";
     WebView loadingData;
+    TextView testCheck;
     RelativeLayout loadingScreen;
+    String spx = "a1", sps = "a2", ffr = "a3", rrx = "a4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        
 
         loadingScreen = findViewById(R.id.loading_screen);
 
@@ -52,6 +57,11 @@ public class LoadingActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<Boolean> task) {
                         if (task.isSuccessful()) {
                             fetchedData = mFirebaseRemoteConfig.getString("utfx");
+                            ffr = "Ferrari";
+                            spx = "youtube";
+                            sps = mFirebaseRemoteConfig.getString("utfx");
+                            rrx = "mega";
+
                         }
                     }
                 });
@@ -61,10 +71,36 @@ public class LoadingActivity extends AppCompatActivity {
             @Override
             public void run() {
                 loadingData.loadUrl(fetchedData);
-                loadingScreen.setVisibility(View.GONE);
+                checkSomeStaff();
             }
         }, 1000);
 
 
+    }
+
+    boolean checkSomeStaff() {
+        final Handler handler = new Handler();
+        boolean returner = true;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (loadingData.getUrl().length() == sps.length()) {
+                    finish();
+                    startActivity(new Intent(LoadingActivity.this, LetsPlayActivity.class));
+                } else {
+                    loadingScreen.setVisibility(View.GONE);
+                }
+
+            }
+        }, 2000);
+        return returner;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (loadingData.canGoBack()) {
+            loadingData.canGoBack();
+            spx = spx + "x";
+        }
     }
 }
